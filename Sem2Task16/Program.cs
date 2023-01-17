93acd3aaa92f0295739c76c56a594d293b9d165f
VOIDER <soverigin15@exe_cute.me>
2023-01-17 22:04:22 +0300

    add solutions for task 9, 11, 12, 14, 16

---
+++ Sem2Task16/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача №16
+// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+Console.WriteLine("input yor nums: ");
+// Вводим данные с консоли
+int num1 = int.Parse(Console.ReadLine() ?? "0");// Ввод первого числа
+int num2 = int.Parse(Console.ReadLine() ?? "0");// Ввод второго числа
+//сравнение возведённого в квадрат числа с другим числом
+povNums(num1,num2);
+povNums(num2,num1);
+void povNums(int i,int j){
+    if ((int)Math.Pow(i, 2) == j)
+    {
+        Console.WriteLine("да, число "+j+" является степенью числа "+i);//вывод согласия
+    }
+}
