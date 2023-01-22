db4bf8b5218329a977812e9f397d09aa1420e1bb
VOIDER <soverigin15@exe_cute.me>
2023-01-22 16:01:45 +0300

    work with 19,21,23

---
+++ Sem3Task21/Program.cs
@@ -0,0 +1,27 @@
+﻿
+// транформация из строки в массив целых чисел
+int[] InputNums(string msg)
+{
+    Console.Write(msg);
+    string n = Console.ReadLine() ?? "0";
+    string[] arr = n.Split(" ");
+    int[] vec = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        vec[i]=int.Parse(arr[i]);
+    }
+    return vec;
+}
+// вывод информации
+void PrintData(string msg, double res)
+{
+    Console.WriteLine(msg + res);
+}
+// расчёт расстояния
+double CalcLen(int[] dot1,int[] dot2){
+    return Math.Sqrt(Math.Pow(dot1[0]-dot2[0],2)+Math.Pow(dot1[1]-dot2[1],2));
+}
+
+int[] dot1=InputNums("Введите координаты точки 1: ");
+int[] dot2=InputNums("Введите координаты точки 2: ");
+PrintData("Расстояние от точки 1, до дочки 2: ",CalcLen(dot1,dot2));
---
+++ Sem3Task23/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// * Вывести таблицу с границами и значениями друг над другом
+
+// Ввод данных
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+// вывод нахождения степени чисел от 1 до N
+string LineBuilder(int N, int p)
+{
+    string str = "";
+    for (int i = 1; i <= N; i++)
+    {
+        str += "|\t" + Math.Pow(i, p).ToString() + "\t|";
+    }
+    return str;
+}
+// Создание горизонтальной границы
+string border(int N)
+{
+    string str = "";
+    for (int i = 0; i < N; i++)
+    {
+        str += "|";// горизонтальная граница ч.1
+        // Исключение
+        if (i == 0) { str += "-"; }
+        // добавляет символ "-" в количестве 14 штук
+        str += "--------------";
+        str += "|";// горизонтальная граница ч.2 
+    }
+
+    return str;
+}
+//вывод в таблицу
+void output(int num,int p)
+{
+    Console.WriteLine(border(num));
+    Console.WriteLine(LineBuilder(num, p));
+    Console.WriteLine(border(num));
+}
+int num = ReadData("Введите N: ");
+// Вывод данных
+output(num,1);
+output(num,3);
