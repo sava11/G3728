0c6f210a3767d76fa510b031c1f927b936dc2b6f
VOIDER <soverigin15@exe_cute.me>
2023-02-11 13:26:07 +0300

    Tsak62Spiral

--- Sem8Task62/Program.cs
+++ Sem8Task62/Program.cs
@@ -1,38 +1,57 @@
-﻿int N=4;
-int M=4;
-int[,] a = new int[N,M];
+﻿
+int[,] Create2DArray(int N, int M)
+{
+    return new int[N, M];
+}
 //
-int Ibeg = 0, Ifin = 0, Jbeg = 0, Jfin = 0;
-
-int k = 1;
-int i = 0;
-int j = 0;
-
-while (k <= N * M)
+void FillSpiral2DAerray(int[,] arr)
 {
-    a[i,j] = k;
-    if (i == Ibeg && j < M - Jfin - 1)
-        ++j;
-    else if (j == M - Jfin - 1 && i < N - Ifin - 1)
-        ++i;
-    else if (i == N - Ifin - 1 && j > Jbeg)
-        --j;
-    else
-        --i;
+    int Ibeg = 0, Ifin = 0, Jbeg = 0, Jfin = 0;
 
-    if ((i == Ibeg + 1) && (j == Jbeg) && (Jbeg != M - Jfin - 1))
+    int k = 1;
+    int i = 0;
+    int j = 0;
+
+    while (k <= arr.GetLength(0) * arr.GetLength(1))
     {
-        ++Ibeg;
-        ++Ifin;
-        ++Jbeg;
-        ++Jfin;
+        arr[i, j] = k;
+        if (i == Ibeg && j < arr.GetLength(1) - Jfin - 1)
+            ++j;
+        else if (j == arr.GetLength(1) - Jfin - 1 && i < arr.GetLength(0) - Ifin - 1)
+            ++i;
+        else if (i == arr.GetLength(0) - Ifin - 1 && j > Jbeg)
+            --j;
+        else
+            --i;
+
+        if ((i == Ibeg + 1) && (j == Jbeg) && (Jbeg != arr.GetLength(1) - Jfin - 1))
+        {
+            ++Ibeg;
+            ++Ifin;
+            ++Jbeg;
+            ++Jfin;
+        }
+        ++k;
     }
-    ++k;
+}
+void Show2DArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); ++i)
+    {
+        for (int j = 0; j < arr.GetLength(1); ++j)
+            Console.Write(arr[i, j] + " \t");
+        Console.WriteLine("");
+    }
+}
+int InputNum(string msg = "")
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
 }
 
-for (i = 0; i < N; ++i)
-{
-    for (j = 0; j < M; ++j)
-        Console.Write(a[i,j]+" \t");
-    Console.WriteLine("");
-}
+int[,]arr=Create2DArray(
+                InputNum("Введите количество строк: "),
+                InputNum("Введите количество колонок: ")
+                );
+FillSpiral2DAerray(arr);
+Show2DArray(arr);
